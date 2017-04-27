74b97aeb48cfbf327e75c0c7751654ab1e6308ef
jon1267 <jon_leev@mail.ru>
2017-04-27 13:52:18 +0300

    Вывод 1-й статьи

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -77,7 +77,10 @@ class ArticlesController extends SiteController
     public function show($alias = false) {
 
         $article = $this->a_rep->one($alias, ['comments' => true]);
-        dd($article);
+
+        if ($article) {
+            $article->img = json_decode($article->img);
+        }
         $content = view(env('THEME').'.article_content')->with('article', $article)->render();
         $this->vars = array_add($this->vars, 'content', $content);
 
